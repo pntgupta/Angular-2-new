22ac2f15c33633082f5d98cc5a38b521382adc38
punit-gupta01 <punit.gupta14@yahoo.com>
2016-09-25 13:04:55 +0530

    aa
    a

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: '<h>aaa</h>'
+})
+class AppComponent{}
+
+bootstrap(AppComponent)
